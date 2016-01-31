bf55c6148be6a0a0b27cf5dfef8c03feda1c765b
BurakAy <burak@burak-aydemir.com>
2016-01-30 19:26:51 -0600

    updated comments

--- simple_server.rb
+++ simple_server.rb
@@ -1,9 +1,9 @@
-require "socket"
+require "socket" # Sockets from stdlib (standard library)
 
-server = TCPServer.open(2000)
+server = TCPServer.open(2000) # Socket listens on port 2000
 loop {
-	client = server.accept
-	client.puts(Time.now.ctime)
+	client = server.accept # Wait for client to connect. Returns TCPSocket representing connection
+	client.puts(Time.now.ctime) # Send time to client, change time
 	client.puts "Closing the connection. Bye!"
 	client.close
 }
